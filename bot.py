dcb367e0f4605e4d8189cd027765c29f439eb1cb
vinsxid <101071339+vinsxid@users.noreply.github.com>
2023-02-20 02:03:35 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,58 +24,58 @@ class Bot(Client):
         db = Database(bot_me.id)
         os.system('cls')
         if not await db.cek_user_didatabase():
-            print(f'[!] Menambahkan data bot ke database...')
+            print(f'[!] ⚙️ Menambahkan data Bot ke Database...')
             await db.tambah_databot()
-        print("[!] Database telah ready")
-        print(f"[!] Link Database Kamu : {config.db_url}")
+        print("[!] ✅ Database telah Ready")
+        print(f"[!] ✅ Link Database : {config.db_url}")
         print("================")
 
         if config.channel_1:
             try:
                 await self.export_chat_invite_link(config.channel_1)
             except:
-                print(f'Harap periksa kembali ID [ {config.channel_1} ] pada channel 1')
-                print(f'Pastikan bot telah dimasukan kedalam channel dan menjadi admin')
-                print('-> Bot terpaksa dihentikan')
+                print(f'ℹ️ Harap periksa kembali ID [ {config.channel_1} ] pada channel 1')
+                print(f'ℹ️ Pastikan bot telah dimasukan kedalam channel dan menjadi admin')
+                print('-> ❌ Bot terpaksa dihentikan')
                 sys.exit()
         if config.channel_2:
             try:
                 await self.export_chat_invite_link(config.channel_1)
             except:
-                print(f'Harap periksa kembali ID [ {config.channel_2} ] pada channel 2')
-                print(f'Pastikan bot telah dimasukan kedalam channel dan menjadi admin')
-                print('-> Bot terpaksa dihentikan')
+                print(f'ℹ️ Harap periksa kembali ID [ {config.channel_2} ] pada channel 2')
+                print(f'ℹ️ Pastikan bot telah dimasukan kedalam channel dan menjadi admin')
+                print('-> ❌ Bot terpaksa dihentikan')
                 sys.exit()
         if config.channel_log:
             try:
                 await self.export_chat_invite_link(config.channel_log)
             except:
-                print(f'Harap periksa kembali ID [ {config.channel_log} ] pada channel log')
-                print(f'Pastikan bot telah dimasukan kedalam channel dan menjadi admin')
-                print('-> Bot terpaksa dihentikan')
+                print(f'ℹ️ Harap periksa kembali ID [ {config.channel_log} ] pada channel log')
+                print(f'ℹ️ Pastikan bot telah dimasukan kedalam channel dan menjadi admin')
+                print('-> ❌ Bot terpaksa dihentikan')
                 sys.exit()
 
         self.username = bot_me.username
         self.id_bot = bot_me.id
         data.append(self.id_bot)
         await self.set_bot_commands([
-            BotCommand('status', '🍃 check status')#, BotCommand('Support', 'Akun Support'),
+            BotCommand('status', '🍃 check status')#, BotCommand('talent', '🌟 talent'),
             #BotCommand('daddysugar', '👔 daddy sugar trusted'), BotCommand('moansgirl', '🧘‍♀️ moans girl'),
             #BotCommand('moansboy', '🧘 moans boy'), BotCommand('gfrent', '🤵 girl friend rent'),
             #BotCommand('bfrent', '🤵 boy friend rent')
         ], BotCommandScopeAllPrivateChats())
         
-        print('BOT TELAH AKTIF')
+        print('🔥 BOT TELAH AKTIF')
     
     async def stop(self):
         await super().stop()
-        print('BOT BERHASIL DIHENTIKAN')
+        print('✅ BOT BERHASIL DIHENTIKAN')
     
     async def kirim_pesan(self, x: str):
         db = Database(config.id_admin).get_pelanggan()
-        pesan = f'<b>📊 #TOTALUSER ( {db.total_pelanggan} ) PENGGUNA</b>\n'
-        pesan += f'➜ <i>Total user yang mengirim menfess hari ini adalah {x}/{db.total_pelanggan} User Aktif</i>\n'
-        pesan += f'➜ <i>Berhasil DIRESET menjadi 0 Menfess</i>'
+        pesan = f'<b>📊 TOTAL USER ➜ [ {db.total_pelanggan} ] USER</b>\n\n'
+        pesan += f'➜ <i>User mengirim menfess hari ini {x}/{db.total_pelanggan} User Aktif</i>\n'
+        pesan += f'➜ <i>Berhasil DIRESET menjadi 0 Menfess\n\n#RESET</i>'
         url = f'https://api.telegram.org/bot{config.bot_token}'
         a = requests.get(f'{url}/sendMessage?chat_id={config.channel_log}&text={pesan}&parse_mode=HTML').json()
         requests.post(f'{url}/pinChatMessage?chat_id={config.channel_log}&message_id={a["result"]["message_id"]}&parse_mode=HTML')
