bae8764010802cd005af57c7e9ac9f36ae0baa59
vinsxid <101071339+vinsxid@users.noreply.github.com>
2023-02-19 23:31:50 +0700

    Update awal_handler.py

--- plugins/command/awal_handler.py
+++ plugins/command/awal_handler.py
@@ -56,15 +56,15 @@ async def statistik_handler(client: Helper, id_bot: int):
     pesan += f"▪️ Girlfriend rent: {len(bot.gfrent)}\n"
     pesan += f"▪️ Boyfriend rent: {len(bot.bfrent)}\n"
     pesan += f"▪️ Banned: {len(bot.ban)}\n\n"
-    pesan += f"🔰 Status Bot: {'AKTIF' if bot.bot_status else 'NON AKTIF'}</b>"
+    pesan += f"🤖 Status Bot: {'✅ AKTIF' if bot.bot_status else '❌ NON AKTIF'}</b>"
     await client.message.reply_text(pesan, True, enums.ParseMode.HTML)
 
 async def list_admin_handler(helper: Helper, id_bot: int):
     db = Database(helper.user_id).get_data_bot(id_bot)
-    pesan = "<b>Owner bot</b>\n"
-    pesan += "• ID: " + str(config.id_admin) + " | <a href='tg://user?id=" + str(config.id_admin) + "'>Owner bot</a>\n\n"
+    pesan = "<b>👑 Owner</b>\n"
+    pesan += "• ID: " + str(config.id_admin) + " | <a href='tg://user?id=" + str(config.id_admin) + "'>Owner</a>\n\n"
     if len(db.admin) > 0:
-        pesan += "<b>Daftar Admin bot</b>\n"
+        pesan += "<b>👷 Daftar Admin</b>\n"
         ind = 1
         for i in db.admin:
             pesan += "• ID: " + str(i) + " | <a href='tg://user?id=" + str(i) + "'>Admin " + str(ind) + "</a>\n"
@@ -76,7 +76,7 @@ async def list_ban_handler(helper: Helper, id_bot: int):
     if len(db.ban) == 0:
         return await helper.message.reply_text('<i>Tidak ada user dibanned saat ini</i>', True, enums.ParseMode.HTML)
     else:
-        pesan = "<b>Daftar banned</b>\n"
+        pesan = "<b>❌ Daftar Banned</b>\n"
         ind = 1
         for i in db.ban:
             pesan += "• ID: " + str(i) + " | <a href='tg://openmessage?user_id=" + str(i) + "'>( " + str(ind) + " )</a>\n"
@@ -106,7 +106,7 @@ async def gagal_kirim_handler(client: Client, msg: types.Message):
 async def help_handler(client, msg):
     db = Database(msg.from_user.id)
     member = db.get_data_pelanggan()
-    pesan = "Supported commands\n"
+    pesan = "📋 Supported Commands\n\n"
     pesan += '/status — melihat status\n'
     pesan += '/talent — melihat talent\n'
     if member.status == 'admin':
@@ -119,14 +119,14 @@ async def help_handler(client, msg):
         pesan += '/ban — ban user\n'
         pesan += '/unban — unban user\n'
     if member.status == 'owner':
-        pesan += '\n=====OWNER COMMAND=====\n'
+        pesan += '\n=====⛑️ OWNER COMMAND=====\n'
         pesan += '/tf_coin — transfer coin\n'
         pesan += '/settings — melihat settingan bot\n'
         pesan += '/list_admin — melihat list admin\n'
         pesan += '/list_ban — melihat list banned\n'
         pesan += '/stats — melihat statistik bot\n'
         pesan += '/bot — setbot (on|off)\n'
-        pesan += '\n=====FITUR TALENT=====\n'
+        pesan += '\n=====⚡ FITUR TALENT=====\n'
         pesan += '/addtalent — menambahkan talent baru\n'
         pesan += '/addsugar — menambahkan talent daddy sugar\n'
         pesan += '/addgirl — menambahkan talent moans girl\n'
@@ -134,7 +134,7 @@ async def help_handler(client, msg):
         pesan += '/addgf — menambahkan talent girlfriend rent\n'
         pesan += '/addbf — menambahkan talent boyfriend rent\n'
         pesan += '/hapus — menghapus talent\n'
-        pesan += '\n=====BROADCAST OWNER=====\n'
+        pesan += '\n=====📢 BROADCAST OWNER=====\n'
         pesan += '/broadcast — mengirim pesan broadcast kesemua user\n'
         pesan += '/admin — menambahkan admin baru\n'
         pesan += '/unadmin — menghapus admin\n'
