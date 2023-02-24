57c9273ad6f4b9a3d19d8b1447d93e6831553e5d
vinsxid <101071339+vinsxid@users.noreply.github.com>
2023-02-25 01:25:00 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os
 
-api_id = int(os.environ.get("API_ID", "27200202"))
-api_hash = os.environ.get("API_HASH", "d7f267b4e32cb2e86b2e807c1b1f1c0c")
-bot_token = os.environ.get("BOT_TOKEN", "6235791848:AAG2ULtzvHLY06KRLmpeGeu4XxdAkmfU1Z4")
+api_id = int(os.environ.get("API_ID", ""))
+api_hash = os.environ.get("API_HASH", "")
+bot_token = os.environ.get("BOT_TOKEN", "")
 # =========================================================== #
 
-db_url = os.environ.get("DB_URL", "mongodb+srv://fess1:fess1@cluster0.faqansq.mongodb.net/?retryWrites=true&w=majority")
-db_name = os.environ.get("DB_NAME", "menfes1")
+db_url = os.environ.get("DB_URL", "")
+db_name = os.environ.get("DB_NAME", "telegram")
 # =========================================================== #
 
 #Channel Utama sambungin ke Grup Base
-channel_1 = int(os.environ.get("CHANNEL_1", "-100"))
+channel_1 = int(os.environ.get("CHANNEL_1", ""))
 #Grup Base
-channel_2 = int(os.environ.get("CHANNEL_2", "0"))
+channel_2 = int(os.environ.get("CHANNEL_2", ""))
 #Channel Log sambungin ke Grup Log (Private)
-channel_log = int(os.environ.get("CHANNEL_LOG", "-1001811659938"))
+channel_log = int(os.environ.get("CHANNEL_LOG", ""))
 # =========================================================== #
 
 id_admin = int(os.environ.get("ID_ADMIN", "1243599890"))
